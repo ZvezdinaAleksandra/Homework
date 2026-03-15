504a8529e925b744079983bd3f81bb1ead9bf5d5
Lil Momma <karyz230921@gmail.com>
2026-03-15 21:26:24 +0300

    homework1 added

---
+++ src/homework1/Homework1_4.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class Homework1_4 {
+    public static void main(String[] args) {
+        double n = 8.2;
+        int a = (int)(n + 0.5);
+
+        System.out.println(a);
+    }
+}
---
+++ src/homework1/Homework1_5.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class Homework1_5 {
+    public static void main(String[] args) {
+        int q = 23;
+        int w = 5;
+
+        System.out.println(q % w);
+    }
+}
