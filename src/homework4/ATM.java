c819e8670644c4f5d9c01cdceb1bdaa1baef1380
Lil Momma <karyz230921@gmail.com>
2026-03-26 22:03:39 +0300

    homework3 added

--- src/homework4/ATM.java
+++ src/homework4/ATM.java
@@ -1,9 +1,17 @@
 package homework4;
+/* Создать класс, описывающий банкомат. Набор купюр, находящихся в
+банкомате, должен задаваться тремя свойствами: количеством купюр
+номиналом 20, 50 и 100. Сделать метод для добавления денег в банкомат.
+Сделать функцию, снимающую деньги, которая принимает сумму денег, а
+возвращает булевое значение - успешность выполнения операции. При
+снятии денег, функция должна распечатывать каким количеством купюр
+какого номинала выдаётся сумма. Создать конструктор с тремя параметрами
+-количеством купюр каждого номинала*/
 
 public class ATM {
-    private int banknote20;
-    private int banknote50;
-    private int banknote100;
+    int banknote20;
+    int banknote50;
+    int banknote100;
 
     public ATM(int banknote20, int banknote50, int banknote100) {
         this.banknote20 = banknote20;
@@ -11,25 +19,63 @@ public class ATM {
         this.banknote100 = banknote100;
     }
 
-    public void addBanknote20(int amount) {
+    public boolean addBanknote20(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
         banknote20 += amount;
+        return true;
     }
 
-    public void addBanknote50(int amount) {
+    public boolean addBanknote50(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
         banknote50 += amount;
+        return true;
     }
 
-    public void addBanknote100(int amount) {
+    public boolean addBanknote100(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
         banknote100 += amount;
-    }
-    public void withdrawBBanknote20(int amount) {
-        banknote20 += amount;
-    }
-    public void withdrawBBanknote50(int amount) {
-        banknote50 += amount;
-    }
-    public void withdrawBBanknote100(int amount) {
-        banknote100 += amount;
+        return true;
     }
 
+    public void withdraw(int amount) {
+        if (amount <= 0 || amount % 10 != 0) {
+            System.out.println("Неверная сумма для выдачи.");
+            return;
+        }
+
+        int remaining = amount;
+
+        int used100 = Math.min(remaining / 100, banknote100);
+        remaining -= used100 * 100;
+
+        int used50 = Math.min(remaining / 50, banknote50);
+        remaining -= used50 * 50;
+
+        int used20 = Math.min(remaining / 20, banknote20);
+        remaining -= used20 * 20;
+
+        if (remaining == 0) {
+            // списываем купюры из банкомата
+            banknote100 -= used100;
+            banknote50 -= used50;
+            banknote20 -= used20;
+
+            System.out.print("Выдано купюр: ");
+            if (used100 > 0) System.out.print("100-" + used100 + " ");
+            if (used50 > 0) System.out.print("50-" + used50 + " ");
+            if (used20 > 0) System.out.print("20-" + used20 + " ");
+            System.out.println();
+        } else {
+            System.out.println("Невозможно выдать точную сумму.");
+        }
+    }
 }
--- src/homework4/MainATM.java
+++ src/homework4/MainATM.java
@@ -3,9 +3,29 @@ package homework4;
 public class MainATM {
     public static void main(String[] args) {
         ATM atm = new ATM(20000, 4000, 10000);
-        atm.addBanknote20(1000);
-        atm.addBanknote50(600);
-        atm.addBanknote100(500);
-    }
+        ATM atm1 = new ATM(30000, 50000, 9000);
+        ATM atm2 = new ATM(900, 100, 8000);
 
+        atm.addBanknote20(1000);
+        if (atm.addBanknote20(1000)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Ошибка");
+        }
+        atm.addBanknote50(10000);
+        if (atm.addBanknote50(10000)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Ошибка");
+        }
+
+        atm.addBanknote100(10000);
+        if (atm.addBanknote100(10000)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Ошибка");
+        }
+
+        atm.withdraw(90089000);
+    }
 }
